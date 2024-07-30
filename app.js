fb14f2b6c9af8dcdd7a7be5b834d2879abe6a115
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2024-07-29 18:15:19 -0700

    handlebars (views, layout, partials)

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
+
 app.use(express.static("public"));
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.get("/", (req, res, next) => {
-  console.log("we received a request to /homepage");
-  res.sendFile(__dirname + "/views/home-page.html")
+  res.render("home-page")
 });
 
 app.get("/contact", (req, res, next) => {
-  console.log("we received a request to /contactpage");
-  res.sendFile(__dirname + "/views/contact-page.html")
+    res.render("contact-page")
+});
+
+app.get("/margherita", (req, res, next) => {
+  pizza = {
+    name: "Margherita",
+    price: 12,
+    imgName: "pizza-margherita.jpg",
+    imgAlt: "Margherita Pizza",
+    ingredients: ["mozzarella", "pizza sauce", "basil"]
+  };
+  res.render("product", pizza);
+});
+
+app.get("/veggie", (req, res, next) => {
+  pizza = {
+    name: "Veggie",
+    price: 13,
+    imgName: "pizza-veggie.jpg",
+    imgAlt: "Veggie Pizza",
+    ingredients: ["mozzarella", "pizza sauce", "basil", "mushrooms"]
+  };
+
+  res.render("product", pizza);
+});
+
+app.get("/seafood", (req, res, next) => {
+    pizza = {
+        name: "Seafood",
+        imgName: "pizza-seafood.jpg",
+        imgAlt: "Seafood Pizza",
+        ingredients: ["mozzarella", "pizza sauce", "basil", "seafood"]
+      };
+    
+      res.render("product", pizza);
 });
 
 app.listen(3000);
